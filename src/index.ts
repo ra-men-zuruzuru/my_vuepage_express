c201cf9d43185302cb3cbc620cb46b2622a77032
igada <iga.dai0226@gmail.com>
2025-12-31 19:56:35 +0900

    no message

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ app.use(express.json());
 
 // CORSヘッダー設定
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://myvuepage-gwzlcz0w3-igadai0226-gmailcoms-projects.vercel.app');
+  res.header('Access-Control-Allow-Origin', 'https://myvuepageexpress.vercel.app/api/jsonget');
   res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
